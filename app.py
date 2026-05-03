f695b881e3ef167ad6b0be0d12789073c3caa290
Manishthakur2 <manish6904@gmail.com>
2026-05-03 09:28:08 +0000

    Flask + MYSQL practise project setup

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "not set")
+
+@app.route('/')
+def home():
+    return f"App is now changed and running! DB_HOST: {DB_HOST}"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
